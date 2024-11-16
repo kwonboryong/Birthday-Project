be32f4473b20a4b693f8df731c3ae8ff92df6a74
kwonboryong <kwonboryong@naver.com>
2024-11-16 23:16:15 +0900

    Update: 프로필 스타일링

--- components/background/Background.tsx
+++ components/background/Background.tsx
@@ -14,28 +14,55 @@ export default function Background({ children }: BackgroundProps) {
         <div className={styles.secondBox}>
           <div className={styles.lingBox}>
             <div className={styles.ling}>
-              <img src="/assets/ling.jpg" alt="" className={styles.lingImg} />
+              <img
+                src="/assets/ling.jpg"
+                alt="링 바인더 아이콘"
+                className={styles.lingImg}
+              />
             </div>
             <div className={styles.ling}>
-              <img src="/assets/ling.jpg" alt="" className={styles.lingImg} />
+              <img
+                src="/assets/ling.jpg"
+                alt="링 바인더 아이콘"
+                className={styles.lingImg}
+              />
             </div>
           </div>
           <div className={styles.lingBottomBox}>
             <div className={styles.ling}>
-              <img src="/assets/ling.jpg" alt="" className={styles.lingImg} />
+              <img
+                src="/assets/ling.jpg"
+                alt="링 바인더 아이콘"
+                className={styles.lingImg}
+              />
             </div>
             <div className={styles.ling}>
-              <img src="/assets/ling.jpg" alt="" className={styles.lingImg} />
+              <img
+                src="/assets/ling.jpg"
+                alt="링 바인더 아이콘"
+                className={styles.lingImg}
+              />
             </div>
           </div>
 
           <div className={styles.leftBox}>
-            <p>프로필</p>
+            <div className={styles.profileBox}>
+              <img
+                src="/assets/profile.jpg"
+                alt="프로필 사진"
+                className={styles.profile}
+              />
+              <p className={styles.profileText}>TODAY IS.. 🌞즐거움</p>
+              <p className={styles.profileMessage}>
+                "...왜 울었는데..." <br /> ".......양파 썰다가" <br />
+                "왜 울었냐고" <br /> "양파 때문이라고 대답했어" <br /> "....내가
+                양파냐......." <br />
+              </p>
+                <p className={styles.profileMessageTitle}>- 개기면 죽는다 중 -</p>
+            </div>
           </div>
           <div className={styles.rightBox}>
-            <div className={styles.mainBox}>
-              {children}
-            </div>
+            <div className={styles.mainBox}>{children}</div>
             <div className={styles.sideBox}>
               <Link href={"/"}>
                 <div className={styles.sideMenu}>홈</div>
