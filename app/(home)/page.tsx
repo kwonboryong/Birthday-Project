f4b7a901b6f58713acd2ab89c5944031909c052d
kwonboryong <kwonboryong@naver.com>
2024-11-16 22:14:30 +0900

    Fix: 상자 비율 수정

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,44 +4,5 @@ import styles from "./Home.module.css";
 export default function Home() {
   return (
     <div><h2>dddddd</h2></div>
-    // <div className={styles.container}>
-    //   <div className={styles.firstBox}>
-    //     <div className={styles.secondBox}>
-    //       <div className={styles.lingBox}>
-    //         <div className={styles.ling}>
-    //           <img src="/assets/ling.jpg" alt="" className={styles.lingImg} />
-    //         </div>
-    //         <div className={styles.ling}>
-    //           <img src="/assets/ling.jpg" alt="" className={styles.lingImg} />
-    //         </div>
-    //       </div>
-    //       <div className={styles.lingBottomBox}>
-    //         <div className={styles.ling}>
-    //           <img src="/assets/ling.jpg" alt="" className={styles.lingImg} />
-    //         </div>
-    //         <div className={styles.ling}>
-    //           <img src="/assets/ling.jpg" alt="" className={styles.lingImg} />
-    //         </div>
-    //       </div>
-
-    //       <div className={styles.leftBox}>
-    //         <p>프로필</p>
-    //       </div>
-    //       <div className={styles.rightBox}>
-    //         <div className={styles.sideBox}>
-    //           <Link href={"/"}>
-    //             <div className={styles.sideMenu}>홈</div>
-    //           </Link>
-    //           <Link href={"/messagesFeed"}>
-    //             <div className={styles.sideMenu}>방명록</div>
-    //           </Link>
-    //           <Link href={"/memory"}>
-    //             <div className={styles.sideMenu}>다이어리</div>
-    //           </Link>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
