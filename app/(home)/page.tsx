514069cb3ce9b71772a6e3979c9c2fab3fb7d98d
kwonboryong <kwonboryong@naver.com>
2024-11-16 23:45:10 +0900

    Update: 홈 스타일링

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,6 +3,24 @@ import styles from "./Home.module.css";
 
 export default function Home() {
   return (
-    <div><h2>dddddd</h2></div>
+    <div className={styles.container}>
+      <div className={styles.updateNews}>
+        <p>Updated news</p>
+        <div className={styles.newContent}>
+          <p>[스크랩] ㅇ</p>
+          <p>[스크랩] ㅇ</p>
+          <p>[스크랩] ㅇ</p>
+        </div>
+      </div>
+      
+      <div className={styles.bgm}>
+        <p>BGM</p>
+      </div>
+
+      <div className={styles.miniRoom}>
+        <p>Mini Room</p>
+        <img src="/assets/miniroom.jpg" alt="미니룸 사진" className={styles.miniRoomImg} />
+      </div>
+    </div>
   );
 }
