92845e34b0e262af79123c0979e3d553d6eccc91
kwonboryong <kwonboryong@naver.com>
2024-11-16 22:11:28 +0900

    Create: 데이터

---
+++ constant.js
@@ -0,0 +1,8 @@
+const messages = [
+  { id: 1, name: "오은비", text: '1안녕하세요!' },
+  { id: 2, name: "김시윤", text: '2안녕하세요!' },
+  { id: 3, name: "지가은", text: '3안녕하세요!' },
+  { id: 4, name: "한연서", text: '4안녕하세요!' }
+]
+
+export default messages;
