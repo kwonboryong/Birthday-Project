1eb6f83ed26df26dc94b22198881d3f91337c757
kwonboryong <kwonboryong@naver.com>
2024-11-19 19:42:13 +0900

    Update: 디테일 페이지 스타일링

--- app/memory/[id]/page.tsx
+++ app/memory/[id]/page.tsx
@@ -1,7 +1,30 @@
-export default function MemoryDetail() {
+import memory from "../../../memory";
+import styles from "./MemoryDetail.module.css";
+import { IoReturnUpBackOutline } from "react-icons/io5";
+
+export default function MemoryDetail({ params: { id } }) {
+  const memoryId = parseInt(id);
+
+  const memoryItem = memory.find((item) => item.id === memoryId);
+
+  if (!memoryItem) {
+    return <p>해당 메모리가 없습니다.</p>;
+  }
+
   return (
-    <div >
-      <p>dddd</p>
+    <div className={styles.container}>
+      <div className={styles.contentBox}>
+        <div className={styles.back}>
+          <IoReturnUpBackOutline />
+        </div>
+        <h1 className={styles.title}>{memoryItem.text}</h1>
+        <img
+          src={memoryItem.src}
+          alt={memoryItem.text}
+          className={styles.img}
+        />
+        <p className={styles.description}>{memoryItem.description}</p>
+      </div>
     </div>
   );
 }
--- memory.js
+++ memory.js
@@ -1,13 +1,58 @@
 const memory = [
-  { id: 1, src: "/assets/memory/14.jpg", text: '소녀LA갈비 단체 사진' },
-  { id: 2, src: "/assets/memory/14-1.jpg", text: '은지와 태양 콘서트 가던 날' },
-  { id: 3, src: "/assets/memory/14-2.jpg", text: '중3 소녀LA갈비 단체 사진' },
-  { id: 4, src: "/assets/memory/16.jpg", text: '중3 친구들과...' },
-  { id: 5, src: "/assets/memory/16-1.jpg", text: '중3 어느 날' },
-  { id: 6, src: "/assets/memory/16-2.jpg", text: '중3 어느 날2' },
-  { id: 7, src: "/assets/memory/24.jpg", text: '24살 겨울' },
-  { id: 8, src: "/assets/memory/25.jpg", text: '백세주 먹고 죽을 것 같던 날' },
-  { id: 9, src: "/assets/memory/25-1.jpg", text: '우리의 일본 여행' }
+  {
+    id: 1,
+    src: "/assets/memory/14.jpg",
+    text: '소녀LA갈비 단체 사진',
+    description: "dsdfs"
+  },
+  {
+    id: 2,
+    src: "/assets/memory/14-1.jpg",
+    text: '은지와 태양 콘서트 가던 날',
+    description: "dsdfs"
+  },
+  {
+    id: 3,
+    src: "/assets/memory/14-2.jpg",
+    text: '중3 소녀LA갈비 단체 사진',
+    description: "dsdfssdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" 
+  },
+  {
+    id: 4,
+    src: "/assets/memory/16.jpg",
+    text: '중3 친구들과...',
+    description: "dsdfs"
+  },
+  {
+    id: 5,
+    src: "/assets/memory/16-1.jpg",
+    text: '중3 어느 날',
+    description: "dsdfs"
+  },
+  {
+    id: 6,
+    src: "/assets/memory/16-2.jpg",
+    text: '중3 어느 날2',
+    description: "dsdfs"
+  },
+  {
+    id: 7,
+    src: "/assets/memory/24.jpg",
+    text: '24살 겨울',
+    description: "dsdfs"
+  },
+  {
+    id: 8,
+    src: "/assets/memory/25.jpg",
+    text: '백세주 먹고 죽을 것 같던 날',
+    description: "dsdfs"
+  },
+  {
+    id: 9,
+    src: "/assets/memory/25-1.jpg",
+    text: '우리의 일본 여행',
+    description: "dsdfs"
+  }
 ]
 
 export default memory;
