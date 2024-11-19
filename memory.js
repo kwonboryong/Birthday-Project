fecb90ce779d305d07f3878a51053f531f47e376
kwonboryong <kwonboryong@naver.com>
2024-11-19 19:02:00 +0900

    Fix: 데이터 텍스트 수정

--- memory.js
+++ memory.js
@@ -1,13 +1,13 @@
 const memory = [
-  { id: 1, src: "/assets/memory/14.jpg", text: '1안녕하세요!' },
-  { id: 2, src: "/assets/memory/14-1.jpg", text: '2안녕하세요!' },
-  { id: 3, src: "/assets/memory/14-2.jpg", text: '3안녕하세요!' },
-  { id: 4, src: "/assets/memory/16.jpg", text: '4안녕하세요!' },
-  { id: 5, src: "/assets/memory/16-1.jpg", text: '5안녕하세요!' },
-  { id: 6, src: "/assets/memory/16-2.jpg", text: '6안녕하세요!' },
-  { id: 7, src: "/assets/memory/24.jpg", text: '7안녕하세요!' },
-  { id: 8, src: "/assets/memory/25.jpg", text: '8안녕하세요!' },
-  { id: 9, src: "/assets/memory/25-1.jpg", text: '9안녕하세요!' }
+  { id: 1, src: "/assets/memory/14.jpg", text: '소녀LA갈비 단체 사진' },
+  { id: 2, src: "/assets/memory/14-1.jpg", text: '은지와 태양 콘서트 가던 날' },
+  { id: 3, src: "/assets/memory/14-2.jpg", text: '중3 소녀LA갈비 단체 사진' },
+  { id: 4, src: "/assets/memory/16.jpg", text: '중3 친구들과...' },
+  { id: 5, src: "/assets/memory/16-1.jpg", text: '중3 어느 날' },
+  { id: 6, src: "/assets/memory/16-2.jpg", text: '중3 어느 날2' },
+  { id: 7, src: "/assets/memory/24.jpg", text: '24살 겨울' },
+  { id: 8, src: "/assets/memory/25.jpg", text: '백세주 먹고 죽을 것 같던 날' },
+  { id: 9, src: "/assets/memory/25-1.jpg", text: '우리의 일본 여행' }
 ]
 
 export default memory;
